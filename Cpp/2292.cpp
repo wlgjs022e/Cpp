812e65e4be9c47ffc24aad709a7e480697f046ec
wlgjs022e <wlgjs0223@gmail.com>
2022-03-22 09:31:09 +0900

    초기 커밋.

---
+++ Cpp/2292.cpp
@@ -0,0 +1,40 @@
+/*#include<iostream>
+
+using namespace std;
+
+
+//int main() {
+
+	int a;
+	int cnt = 1;
+
+	// 1 6 12 18 24 
+	cin >> a;
+
+	if (a == 1) {
+		cout << cnt;
+	}
+	else if (a >= 2 && a <= 7) {
+		cnt += 1;
+		cout << cnt; 
+	}
+	else if (a >= 8 && a <= 19) {
+		cnt += 2;
+		cout << cnt;
+	}
+	else if (a >= 20 && a <= 37) {
+		cnt += 3;
+		cout << cnt;
+	}
+	else if (a >= 38 && a <= 61) {
+		cnt += 4;
+		cout << cnt;
+	}
+	else {
+		cnt += 5;
+		cout << cnt;
+	}
+
+
+	return 0;
+}*/
