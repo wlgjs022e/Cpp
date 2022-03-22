43297d00516caa9f50c941086b774af942d13445
wlgjs022e <wlgjs0223@gmail.com>
2022-03-22 14:49:00 +0900

    1

--- Cpp/0322_4.cpp
+++ Cpp/0322_4.cpp
@@ -1,13 +1,12 @@
 //#include<iostream>
 //#include<cstring>
-//
 //using namespace std;
 //
 //
 //class Movie{
 //
 //private:
-//	string n;// char n[25];// string n = "";
+//	string n;
 //	double number;
 //
 //
@@ -18,38 +17,39 @@
 //		number = b;
 //	}
 //	
-//	char getprint();
+//	string getprint();
 //	double getprint1();
 //
 //};
 //
-//char Movie::getprint() {
-//	
-//	cout << n; 
+//string Movie::getprint() {
+//	 
+//	return n;
 //}
 //
 //double Movie::getprint1() {
-//	cout << number;
+//	
+//	return number;
 //}
 //
 //int main() {
 //
 //
-//	Movie mv[3] = {Movie("스파이더맨",8.9) , Movie("이상한 나라의 수학자",7.3) , 
-//		Movie("더 배트맨",9.5) };
+//	Movie mv[3] = {Movie("스파이더맨",8.9) , Movie("이상한 나라의 수학자",7.3) ,  Movie("더 배트맨",9.5) };
 //
+//	
 //
 //	for (int i = 0; i < 3; i++) {
-//		cout << "영화명" << mv[i].getprint() << " , 평점 : " << mv[i].getprint1();
+//		cout << "영화명 : " << mv[i].getprint() << " , 평점 : " << mv[i].getprint1() << endl;
 //	}
 //	
 //	Movie *mp;
-//	mp = mv;
+//	mp = mv; // mp 에 mv 의 주소가 들어감 
 //
 //	for (int i = 0; i < 3; i++) {
 //		
-//		cout << "영화명" << mp->getprint() << " , 평점 : " << mp->getprint1();
-//		mp++;
+//		cout << "영화명 : " << mp->getprint() << " , 평점 : " << mp->getprint1() << endl;
+//		mp++; // 주소 + 1씩함 
 //	}
 //	return 0;
 //}
