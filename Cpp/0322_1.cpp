6ecbc01318f0fb200eda0a8c534ae8167b1d1010
wlgjs022e <wlgjs0223@gmail.com>
2022-03-22 09:32:57 +0900

    test

--- Cpp/0322_1.cpp
+++ Cpp/0322_1.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 
 using namespace std;
 
@@ -33,4 +33,4 @@ int main() {
 
 
 	return 0;
-}*/
+}
--- Cpp/1712.cpp
+++ Cpp/1712.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 
 using namespace std;
 
@@ -18,4 +18,4 @@ int main() {
 
 
 	return 0;
-}
+}*/
